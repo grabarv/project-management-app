ce2febc6e8869c4c102cd7c455a9ae92bc153d5a
grabarv <grabar.vladyslav.nl@gmail.com>
2026-02-08 18:24:06 +0100

    change button style

--- frontend/project-management-ui/src/components/WelcomePage.jsx
+++ frontend/project-management-ui/src/components/WelcomePage.jsx
@@ -4,7 +4,7 @@ export default function WelcomePage() {
   return (
     <div className="welcome-page">
       <div className="welcome-card">
-        <h1>Welcome to Projects Manager</h1>
+        <h1>Welcome to Projects Manager App</h1>
 
         <div className="button-group">
           <button className="sign-in">Sign in</button>
